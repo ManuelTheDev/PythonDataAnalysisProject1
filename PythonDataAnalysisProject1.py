044f512f9d9c115168a775b2c6e6b6ca52509129
Manubilo <mjdelgado@pucp.pe>
2021-10-05 22:17:13 -0500

    ctrl + z

--- PythonDataAnalysisProject1.py
+++ PythonDataAnalysisProject1.py
@@ -8,8 +8,3 @@ pwd = os.getcwd()
 
 # Reading csv file
 df = pd.read_csv(pwd + '/data.csv')
-
-# Filtering dataframe
-df_payments = df[df.id_client > 100]
-df_payments = df_payments[df_payments.id_cliente % 20 == 0]
-df_payments = df_payments[['id_cliente', 'revenue']]
