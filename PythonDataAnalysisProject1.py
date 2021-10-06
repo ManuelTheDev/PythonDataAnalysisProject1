352c38f8c5a3a157dddc5b68b2d0dfa20df33e3f
Manubilo <mjdelgado@pucp.pe>
2021-10-05 22:18:04 -0500

    adding filtering dataframe

--- PythonDataAnalysisProject1.py
+++ PythonDataAnalysisProject1.py
@@ -8,3 +8,8 @@ pwd = os.getcwd()
 
 # Reading csv file
 df = pd.read_csv(pwd + '/data.csv')
+
+# Filtering dataframe
+df_payments = df[df.id_client > 100]
+df_payments = df_payments[df_payments.id_cliente % 20 == 0]
+df_payments = df_payments[['id_cliente', 'revenue']]
